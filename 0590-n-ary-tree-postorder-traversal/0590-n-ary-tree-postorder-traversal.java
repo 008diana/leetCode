63be9185c71ee9b5e12e0dad8eed4888c38cb7de
008diana <115479775+008diana@users.noreply.github.com>
2024-08-26 16:00:57 +0530

    Time: 1 ms (34.54%), Space: 45.5 MB (6.04%) - LeetHub

---
+++ 0590-n-ary-tree-postorder-traversal/0590-n-ary-tree-postorder-traversal.java
@@ -0,0 +1,35 @@
+// a little confused as to how we know what order the children are in, but fine
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    public List<Integer> postorder(Node root) {
+        ArrayList<Integer> list = new ArrayList<>();
+        if (root == null) {
+            return list;
+        }
+
+        for (int i = 0; i < root.children.size(); i++) {
+            list.addAll(postorder(root.children.get(i)));
+        }
+        list.add(root.val);
+        return list;
+        
+    }
+}
