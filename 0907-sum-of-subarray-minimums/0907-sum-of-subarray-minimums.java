a9f4eabd7411583c3b99c836d6b946a4009669e5
008diana <115479775+008diana@users.noreply.github.com>
2024-12-20 12:51:27 +0530

    Time: 91 ms (5.09%), Space: 55.2 MB (5.87%) - LeetHub

---
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.java
@@ -0,0 +1,62 @@
+class Solution {
+    public int sumSubarrayMins(int[] arr) {
+
+        int n = arr.length;
+        int mod = (int)Math.pow(10, 9) + 7;
+        int sum = 0;
+        int[] right = new int[n];
+        right = nextSmallestElement(arr);
+        int[] left = new int[n]; 
+        left = previousSmallestOrEqualElement(arr);
+
+        for(int i = 0; i < n; i++){
+            long contribution = ((long)(right[i] - i) * (i - left[i]) % mod) * arr[i] % mod; // Use modulo
+            sum = (sum + (int)contribution) % mod; // Update sum with modulo
+        }
+
+        return sum % mod;        
+
+    }
+
+    public static int[] nextSmallestElement(int[] arr){
+        int n = arr.length;
+        int[] ans = new int[n];
+        HashMap<Integer, Integer> map = new HashMap<>();
+        Stack<Integer> stk = new Stack<>();
+        stk.push(0);
+        for(int i = 1; i < n; i++){
+            while(!stk.isEmpty() && arr[stk.peek()] > arr[i]){
+                map.put(stk.pop(), i);
+            }
+            stk.push(i);
+        }
+        while(!stk.isEmpty()){
+            map.put(stk.pop(), n);
+        }
+        for(int i = 0; i < n; i++){
+            ans[i] = map.get(i);
+        }
+        return ans;
+    }
+
+    public static int[] previousSmallestOrEqualElement(int[] arr){
+        int n = arr.length;
+        int[] ans = new int[n];
+        HashMap<Integer, Integer> map = new HashMap<>();
+        Stack<Integer> stk = new Stack<>();
+        stk.push(n-1);
+        for(int i = n-2; i >= 0; i--){
+            while(!stk.isEmpty() && arr[stk.peek()] >= arr[i]){
+                map.put(stk.pop(), i);
+            }
+            stk.push(i);
+        }
+        while(!stk.isEmpty()){
+            map.put(stk.pop(), -1);
+        }
+        for(int i = 0; i < n; i++){
+            ans[i] = map.get(i);
+        }
+        return ans;
+    }
+}
