d815d323f01006b5d70cb0243c4a4681f4ccb7c3
008diana <115479775+008diana@users.noreply.github.com>
2024-12-20 13:26:09 +0530

    Time: 40 ms (68.30%), Space: 49.4 MB (84.47%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.java
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.java
@@ -1,3 +1,30 @@
+//Thanks to Dhruv Ladani, yeh optimal approach mila. love the way he computes both nse, pse at the same time 
+class Solution {
+    public int sumSubarrayMins(int[] arr) {
+        final int MOD = 1000000007;
+        Stack<Integer> st = new Stack<>();
+        long sumOfMinimums = 0;
+
+        for (int i = 0; i <= arr.length; i++) {
+            while (!st.empty() && (i == arr.length || arr[st.peek()] >= arr[i])) {
+                int mid = st.pop();
+                int leftBoundary = st.empty() ? -1 : st.peek();
+                int rightBoundary = i;
+
+                long count = (mid - leftBoundary) * (rightBoundary - mid) % MOD;
+
+                sumOfMinimums += (count * arr[mid]) % MOD;
+                sumOfMinimums %= MOD;
+            }
+            st.push(i);
+        }
+
+        return (int) sumOfMinimums;
+    }
+}
+
+/*
+-----------------------MY SOLUTION BASED ON STRIVER'S EXPLAINATION-------------------------
 class Solution {
     public int sumSubarrayMins(int[] arr) {
 
@@ -60,3 +87,4 @@ class Solution {
         return ans;
     }
 }
+*/
