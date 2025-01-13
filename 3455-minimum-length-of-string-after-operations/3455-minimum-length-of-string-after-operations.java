e174fbebf08b5da7c45b0a9c427edfe1d395bf22
008diana <115479775+008diana@users.noreply.github.com>
2025-01-13 12:43:16 +0530

    Time: 29 ms (44.37%), Space: 46.4 MB (53.64%) - LeetHub

--- 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.java
+++ 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.java
@@ -1,15 +1,4 @@
 class Solution {
-    // Helper method to print the array
-    private void print(int[] arr) {
-        System.out.print("Array values: [");
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i]);
-            if (i < arr.length - 1) {
-                System.out.print(", ");
-            }
-        }
-        System.out.println("]");
-    }
 
     public int minimumLength(String s) {
         int n = s.length();
@@ -18,7 +7,6 @@ class Solution {
         for(int i = 0; i < n; i++){
             char ch = s.charAt(i);
             arr[ch - 'a']++;
-            //print(arr);
             if(arr[ch -'a'] == 3){
                 length -= 2;
                 arr[ch - 'a'] -= 2;
