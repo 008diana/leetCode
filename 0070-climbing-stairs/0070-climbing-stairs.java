2c2990ecbe0fe4e217a4610a655304880e0d132c
008diana <115479775+008diana@users.noreply.github.com>
2025-03-07 16:03:53 +0530

    Time: 0 ms (100%), Space: 40.3 MB (64.67%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,12 +1,21 @@
-//DP trial
 class Solution {
     public int climbStairs(int n) {
-        int[] ans = new int [45];
-        ans[0] = 1;
-        ans[1] = 2;
-        for (int i = 2; i < 45; i++) {
-            ans[i] = ans[i-1] + ans[i -2];
+        int[] memo = new int[n + 1];
+        Arrays.fill(memo, -1);
+        return countWays(n, memo);
+    }
+
+    public static int countWays(int n, int[] memo){
+        if(n == 0){
+            return 1; 
         }
-        return ans[n - 1];
+        if(n == 1){
+            return 1;
+        }
+        
+        if(memo[n] != -1){
+            return memo[n];
+        }
+        return memo[n] = countWays(n - 2, memo) + countWays(n - 1, memo);
     }
 }
