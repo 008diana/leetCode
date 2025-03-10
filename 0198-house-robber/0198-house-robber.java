a4e2444c70a3a82b69a4fa2e5a519e7a9ab1e908
008diana <115479775+008diana@users.noreply.github.com>
2025-03-10 11:42:36 +0530

    Time: 0 ms (100%), Space: 40.9 MB (76.31%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,39 +1,34 @@
-//undersatnding slowly slowly
 class Solution {
     public int rob(int[] nums) {
-    if (nums.length == 0) return 0;
-    if (nums.length == 1) return nums[0];
-    
-    int[] dp = new int[nums.length];
-    dp[0] = nums[0];
-    dp[1] = Math.max(nums[0], nums[1]);
-    
-    for (int i = 2; i < nums.length; i++) {
-        dp[i] = Math.max(dp[i-1], nums[i] + dp[i-2]);
-    }
-    
-    return dp[nums.length - 1];
-    }
-}
-//my try
-/*
-    public int rob(int[] nums) {
         int n = nums.length;
-        if(n == 1) {
+        if(nums.length == 1){
             return nums[0];
         }
-        if(n == 2) {
-            Arrays.sort(nums);
-            return nums[1];
+        if(nums.length == 2){
+            return Math.max(nums[0], nums[1]);
         }
-        int[] dp = new int [n];
-        dp[0] = nums[0];
-         dp[1] = Math.max(nums[0], nums[1]);
-        for(int i = 2; i < n; i++) {
-            dp[i] = Math.max(nums[i] + dp[i - 2], nums[i - 1]);// error here, had to be dp[i-1] instead of nums[i-1]
-        } 
-        return dp[n -1];
+        if(nums.length == 3){
+            return Math.max(nums[0] + nums[2], nums[1]);
+        }
+        int[] memo = new int[n+1];
+        for(int i = 0; i < n + 1; i++){
+            memo[i] = -1;
+        }
+        return maxMoney(nums, n - 1, memo);
     }
-    
+
+    public static int maxMoney (int[] nums, int index,int[] memo){
+        if (index < 0) {
+            return 0;
+        }
+        if(memo[index] != -1){
+            return memo[index];
+        }
+
+        int left = nums[index] + maxMoney(nums, index - 2, memo);
+        int right =  maxMoney(nums, index - 1, memo);       
+
+        return memo[index] = Math.max(left, right);
+
+    }
 }
-*/
