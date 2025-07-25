3775539ce2b89119d59c9ff9529420fe1a369b04
008diana <115479775+008diana@users.noreply.github.com>
2025-07-25 23:22:02 +0530

    Time: 1 ms (100%), Space: 44.4 MB (67.94%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -10,50 +10,30 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        boolean flag1 = false;
-        boolean flag2 = false;
+        ListNode dummyHead = new ListNode(0); // Start with dummy
+        ListNode curr = dummyHead;
         int carry = 0;
-        int ans = 0;
-        ListNode curr3 = new ListNode(0);
-        ListNode head3 = curr3;
-        ListNode curr1 = l1;
-        ListNode curr2 = l2;
 
-        while (curr1 != null || curr2 != null) {
-            if (curr1 == null) {
-                flag1 = true;
-                ans = curr2.val + carry;
-            } 
-            else if (curr2 == null) {
-                flag2 = true;
-                ans = curr1.val + carry;
-            } 
-            else {
-                ans = curr1.val + curr2.val + carry;
-            }
+        // Traverse both lists
+        while (l1 != null || l2 != null) {
+            int val1 = (l1 != null) ? l1.val : 0;
+            int val2 = (l2 != null) ? l2.val : 0;
 
-            if (ans > 9) {
-                ans = ans - 10;
-                carry = 1;
-            } else {
-                carry = 0;
-            }
+            int sum = val1 + val2 + carry;
+            carry = sum / 10;
 
-            curr3.next = new ListNode(ans);
-            curr3 = curr3.next;
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
 
-            if (!flag1) {
-                curr1 = curr1.next;
-            }
-            if (!flag2) {
-                curr2 = curr2.next;
-            }
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
         }
 
-        if (carry == 1) {
-            curr3.next = new ListNode(1);
+        // Add final carry if needed
+        if (carry > 0) {
+            curr.next = new ListNode(carry);
         }
 
-        return head3.next;
+        return dummyHead.next;
     }
 }
