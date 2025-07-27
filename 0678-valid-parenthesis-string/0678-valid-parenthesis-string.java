606df56ae61f5f0a1f1dd2a41358aae27ae7afe0
008diana <115479775+008diana@users.noreply.github.com>
2025-07-27 15:25:48 +0530

    Time: 0 ms (100%), Space: 41.5 MB (44.05%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
@@ -1,68 +1,37 @@
-// class Solution {
-//     public boolean checkValidString(String s) {
-//         int m = s.lastIndexOf('*');
-//         return combinations(s.length(), s, 0, false, m);
-//     }
-
-//     public static boolean combinations(int length, String s, int index, boolean curr, int lastIndex) {
-//         if (index == length) {
-//             return checkParenthesis(s);
-//         }
-
-//         if (s.charAt(index) != '*') {
-//             return combinations(length, s, index + 1, curr, lastIndex);
-//         }
-
-//         StringBuilder sb = new StringBuilder(s);
-        
-//         // Try '('
-//         sb.setCharAt(index, '(');
-//         boolean a = combinations(length, sb.toString(), index + 1, curr, lastIndex);
-        
-//         // Try ')'
-//         sb.setCharAt(index, ')');
-//         boolean b = combinations(length, sb.toString(), index + 1, curr, lastIndex);
-        
-//         // Try empty
-//         sb.setCharAt(index, ' ');
-//         boolean c = combinations(length, sb.toString(), index + 1, curr, lastIndex);
-        
-//         return a || b || c;
-//     }
-
-//     public static boolean checkParenthesis(String s) {
-//         Stack<Character> stk = new Stack<>();
-//         for (char curr : s.toCharArray()) {
-//             if (curr == ' ') continue;
-//             if (!stk.isEmpty() && stk.peek() == '(' && curr == ')') {
-//                 stk.pop();
-//             } else {
-//                 stk.push(curr);
-//             }
-//         }
-//         return stk.isEmpty();
-//     }
-// }
-
 class Solution {
     public boolean checkValidString(String s) {
-        int minOpen = 0, maxOpen = 0;
-        
-        for (char c : s.toCharArray()) {
-            if (c == '(') {
-                minOpen++;
-                maxOpen++;
-            } else if (c == ')') {
-                minOpen = Math.max(0, minOpen - 1);
-                maxOpen--;
-            } else { // '*'
-                minOpen = Math.max(0, minOpen - 1);
-                maxOpen++;
+        int min = 0; // Minimum open parentheses count
+        int max = 0; // Maximum open parentheses count (including '*')
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (ch == '(') {
+                // Guaranteed open, increase both min and max
+                min++;
+                max++;
+            } else if (ch == ')') {
+                // Guaranteed close, decrease both min and max
+                min--;
+                max--;
+            } else if (ch == '*') {
+                // Could be '(', ')' or ''
+                min--;     // If '*' is ')'
+                max++;     // If '*' is '('
             }
-            
-            if (maxOpen < 0) return false;
+
+            // If max < 0, too many closing brackets
+            if (max < 0) {
+                return false;
+            }
+
+            // min can't be negative, reset to 0
+            if (min < 0) {
+                min = 0;
+            }
         }
-        
-        return minOpen == 0;
+
+        // In the end, min == 0 means all opens are matched
+        return min == 0;
     }
 }
