9fcb15452d8a9a6a1f548351bcdf042285f0a958
008diana <115479775+008diana@users.noreply.github.com>
2025-08-02 08:40:11 +0530

    Time: 51 ms (43.56%), Space: 55.1 MB (62.91%) - LeetHub

---
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int totalFruit(int[] fruits) {
+        int n = fruits.length;
+        int start = 0;
+        int end = 1;
+        int max = 1;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(fruits[0], 1);
+        
+        while (start < n && end < n) {
+            int fruit = fruits[end];
+            
+            if (map.containsKey(fruit)) {
+                int currVal = map.get(fruit);
+                map.put(fruit, currVal + 1);
+                max = Math.max(end - start + 1, max);
+                end++;
+                //System.out.println("Start:" + start + " Max:" + max + " Map:" + map + " End:" + end + " first");
+            } 
+            else if (!map.containsKey(fruit) && map.size() != 2) {
+                max = Math.max(end - start + 1, max);
+                map.put(fruit, 1);
+                end++;
+                //System.out.println("Start:" + start + " Max:" + max + " Map:" + map + " End:" + end + " second");
+            } 
+            else {
+                while (map.size() == 2) {  // ⬅️ fix: run until size == 1 is incorrect
+                    int val = map.getOrDefault(fruits[start], 0);  // ⬅️ fix: use getOrDefault
+                    if (val == 1) {
+                        map.remove(fruits[start]);
+                    } else {
+                        map.put(fruits[start], val - 1);
+                    }
+                    start++;
+                }
+
+                map.put(fruit, 1);
+                max = Math.max(end - start + 1, max);
+                end++;  // ⬅️ fix: missing end++ here
+                //System.out.println("Start:" + start + " Max:" + max + " Map:" + map + " End:" + end + " third");
+            }
+        }
+        return max;
+    }
+}
