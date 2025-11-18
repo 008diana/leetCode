82f065cfb027c01c351f68c32f903bcea75f897b
008diana <115479775+008diana@users.noreply.github.com>
2025-11-19 01:01:31 +0530

    Time: 0 ms (100%), Space: 42 MB (12.41%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,18 +1,40 @@
 class Solution {
     public int climbStairs(int n) {
-        int prev = 1;
-        int prev2 = 1;
-        int curr = prev + prev2;
-        if(n == 0 || n == 1){
-            return 1;
+        //return recursive(n);
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, -1) ;
+        //return memoization(n, dp);
+        //return tabulation(n, dp);
+        return spaceOptimized(n);
+    }
+    public static int recursive(int n){
+        if(n == 0) return 1;
+        if (n == 1) return 1;
+        return recursive(n - 1) + recursive(n- 2);
+    }
+    public static int memoization(int n, int[] dp){
+        if(n <= 1) return 1;
+        if(dp[n] != -1) return dp[n];
+        return dp[n] = memoization(n-1, dp) + memoization(n-2, dp);
+    }
+    public static int tabulation(int n, int[] dp){
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i = 2; i < n + 1; i++){
+            dp[i] = dp[i-1] + dp[i-2];
         }
+        return dp[n];
+    }
+    public static int spaceOptimized(int n){
+        if (n == 0) return 1;
+        if (n == 1) return 1;
+        int prev2 = 1; 
+        int prev = 1; 
         for(int i = 2; i <= n; i++){
-            
-            curr = prev + prev2;
-            prev = prev2;
-            prev2 = curr;
-            
+            int cur_i = prev2 + prev;
+            prev2 = prev;
+            prev = cur_i;
         }
-        return curr;
+        return prev;
     }
 }
